fdb1a1a86777cf6dca4d1fb87313ec2b6c922592
MaksimAka <akavityj@gmail.com>
2025-10-02 15:38:03 +0200

    Lab3

---
+++ LABA3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int x;
+    cin >> x;
+
+    if ((x & 1) == 0) {
+        cout << "Число чётное" << endl;
+    }
+    else {
+        cout << "Число нечётное" << endl;
+    }
+
+    return 0;
+}
